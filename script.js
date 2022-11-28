9bd96674c6b2a332797882828df3abb11326835b
AndresUribe1234 <anduri1997@gmail.com>
2022-11-28 16:25:52 -0500

    refactoring ready

--- script.js
+++ script.js
@@ -8,47 +8,33 @@ class App {
   constructor() {
     this.timer = { hours: 0, minutes: 0, seconds: 0 };
     this.stopwatch;
+    this.target;
     btnContainer.addEventListener("click", this.btnFunctionality.bind(this));
   }
 
   btnFunctionality(e) {
-    const target = e.target;
-    const btn = target.closest(".icon-container");
-
+    this.target = e.target;
+    const btn = this.target.closest(".icon-container");
     if (!btn) return;
-
     if (btn.classList.contains("action-btn")) {
-      btnAction.classList.toggle("running");
-
-      if (btnAction.classList.contains("running")) {
-        this.stopwatch = setInterval(this.btnAction.bind(this), 1000);
-        btnAction.querySelector("img").src =
-          "https://cdn-icons-png.flaticon.com/128/2920/2920686.png";
-      }
-      if (!btnAction.classList.contains("running")) {
-        clearInterval(this.stopwatch);
-        btnAction.querySelector("img").src =
-          "https://cdn-icons-png.flaticon.com/128/27/27223.png";
-      }
+      this.actionBtn();
     }
-
     if (btn.classList.contains("replay-btn")) {
-      this.timer = { hours: 0, minutes: 0, seconds: 0 };
-      time.textContent = `${
-        this.timer.hours < 10 ? "0" + this.timer.hours : this.timer.hours
-      }:${
-        this.timer.minutes < 10 ? "0" + this.timer.minutes : this.timer.minutes
-      }:${
-        this.timer.seconds < 10 ? "0" + this.timer.seconds : this.timer.seconds
-      }`;
-      btnAction.querySelector("img").src =
-        "https://cdn-icons-png.flaticon.com/128/27/27223.png";
-      clearInterval(this.stopwatch);
-      btnAction.classList.toggle("running");
+      this.replayBtn();
     }
   }
 
-  btnAction() {
+  stopwatchInterfaceLogic() {
+    time.textContent = `${
+      this.timer.hours < 10 ? "0" + this.timer.hours : this.timer.hours
+    }:${
+      this.timer.minutes < 10 ? "0" + this.timer.minutes : this.timer.minutes
+    }:${
+      this.timer.seconds < 10 ? "0" + this.timer.seconds : this.timer.seconds
+    }`;
+  }
+
+  intervalCallback() {
     this.timer.seconds++;
     if (this.timer.seconds === 60) {
       this.timer.minutes++;
@@ -58,13 +44,33 @@ class App {
       this.timer.hours++;
       this.timer.minutes = 0;
     }
-    time.textContent = `${
-      this.timer.hours < 10 ? "0" + this.timer.hours : this.timer.hours
-    }:${
-      this.timer.minutes < 10 ? "0" + this.timer.minutes : this.timer.minutes
-    }:${
-      this.timer.seconds < 10 ? "0" + this.timer.seconds : this.timer.seconds
-    }`;
+
+    this.stopwatchInterfaceLogic();
+  }
+
+  actionBtn() {
+    btnAction.classList.toggle("running");
+    if (btnAction.classList.contains("running")) {
+      this.stopwatch = setInterval(this.intervalCallback.bind(this), 1000);
+      btnAction.querySelector("img").src =
+        "https://cdn-icons-png.flaticon.com/128/2920/2920686.png";
+    }
+    if (!btnAction.classList.contains("running")) {
+      clearInterval(this.stopwatch);
+      btnAction.querySelector("img").src =
+        "https://cdn-icons-png.flaticon.com/128/27/27223.png";
+    }
+  }
+
+  replayBtn() {
+    if (btnAction.classList.contains("running")) {
+      btnAction.classList.remove("running");
+    }
+    this.timer = { hours: 0, minutes: 0, seconds: 0 };
+    this.stopwatchInterfaceLogic();
+    btnAction.querySelector("img").src =
+      "https://cdn-icons-png.flaticon.com/128/27/27223.png";
+    clearInterval(this.stopwatch);
   }
 }
 
